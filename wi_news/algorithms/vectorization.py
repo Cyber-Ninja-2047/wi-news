dd1433e596852057f9b3be44515bd7d80ee40af0
Anthony Chow <mrchowpoor@gmail.com>
2024-04-12 22:51:33 +0800

    feat: Finished News Sorting

--- wi_news/algorithms/vectorization.py
+++ wi_news/algorithms/vectorization.py
@@ -6,21 +6,40 @@ Created on Sun Apr  7 11:02:49 2024
 @author: anthony
 """
 import re
-from queue import deque
+from dataclasses import dataclass
+from datetime import datetime
 import numpy as np
 import spacy
 from sentence_transformers import SentenceTransformer
-from wi_news.algorithms.hashtags_extraction import get_pool
+from wi_news.algorithms.hashtags import get_pool
 
 
-REG_OMISSION = re.compile(r'\[\+\d+\schars\]')
-REG_HTMLTAG = re.compile(r'\<.*?\>')
+REG_OMISSION = re.compile(r"\[\+\d+\schars\]")
+REG_REPLACE = re.compile(r"\<.*?\>|\s+")
 try:
     NLP = spacy.load("en_core_web_md")
-except OSError:
-    raise RuntimeError("Please run 'python -m spacy download en_core_web_md' "
-                       "to download the nlp model.")
-LLM = SentenceTransformer('all-MiniLM-L6-v2')
+except OSError as exc:
+    raise RuntimeError(
+        "Please run 'python -m spacy download en_core_web_md' "
+        "to download the nlp model."
+    ) from exc
+LLM = SentenceTransformer("all-MiniLM-L6-v2")
+
+
+@dataclass
+class Article:
+    "data of article"
+    raw: dict
+    text: str
+    lemma: str
+    vector: np.ndarray
+    hashtags_pool: list
+    time: datetime
+
+
+def filter_word(x):
+    "only keep the no-stop words"
+    return not x.is_stop and (x.is_alpha or x.is_digit)
 
 
 def preprocess(article):
@@ -35,17 +54,27 @@ def preprocess(article):
 
     returns
     -------
-    vector : np.array,
-        the vector of the article
-    hashtags_pool : list,
-        possible hashtags
+    data : Article,
+        data of article, including properties below:
+        raw : dict,
+            raw data from news-api
+        text : str,
+            combination of title, description and content of the news
+        vector : np.array,
+            the vector of the article
+        hashtags_pool : list,
+            possible hashtags
 
     """
     # init
-    text = (article['title'] + '. '+
-            article.get('description', '') + '. ' +
-            article.get('content', ''))
-    text = REG_HTMLTAG.sub(' ', text)
+    text = (
+        article["title"]
+        + ". "
+        + article.get("description", "")
+        + ". "
+        + article.get("content", "")
+    )
+    text = REG_REPLACE.sub(" ", text)
 
     # cut the text
     for mat in REG_OMISSION.finditer(text):
@@ -53,6 +82,10 @@ def preprocess(article):
         break
 
     doc = NLP(text)
+
+    # lemma
+    lemma = ' '.join(map(lambda x: x.lemma_,
+                     filter(filter_word, doc)))
 
     # split into sentences
     sentences = []
@@ -69,4 +102,8 @@ def preprocess(article):
     # hashtags pool
     hashtags_pool = get_pool(doc)
 
-    return vector, hashtags_pool
+    published_at = datetime.fromisoformat(article['publishedAt'])
+
+    return Article(raw=article, text=text, lemma=lemma,
+                   vector=vector, hashtags_pool=hashtags_pool,
+                   time=published_at)
